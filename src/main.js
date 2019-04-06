a9deebaa6d5d9db78bd4c72c3a1c2f97cabd7a07
unknown <18357115265@163.com>
2019-04-06 15:16:10 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "Vue"
+
+//全局引入mint-ui
+// import MintUI from 'mint-ui'
+// import 'mint-ui/lib/style.css'
+// Vue.use(MintUI)
+
+//按需导入，让代码体积变小
+import { header } from 'mint-ui';
+Vue.component(header.name, header);
+
+// //导入mui样式
+import "./lib/mui/css/mui.css"
+
+
+import App from "./App.vue"
+const vm = new Vue({
+    el:"#app",
+    render:function(createElement){
+        return createElement(App)
+    }
+})
